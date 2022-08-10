9f3cf5237f971206a7cafb52a249944ce6a0736f
valerio-figueira <j.valerio.figueira@gmail.com>
2022-08-09 23:30:01 -0300

    updating files

--- script/folder.js
+++ script/folder.js
@@ -7,10 +7,28 @@ for (i = 0; i < folder.length; i++) {
     folder[i].addEventListener("click", function() {
     this.classList.toggle("active");
     var content = this.nextElementSibling;
-    if (content.style.display === "block") {
-      content.style.display = "none";
+    if (content.className === "file") {
+      content.className += " open";
+      content.style.maxHeight = "1000px";
     } else {
-      content.style.display = "block";
+      content.className = "file";
+      content.style.maxHeight = "0";
     }
   });
 }
+
+/*
+for (i = 0; i < folder.length; i++) {
+    folder[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+      content.className = "file";
+    } else {
+      content.style.display = "block";
+      content.className += " open";
+    }
+  });
+}
+*/
