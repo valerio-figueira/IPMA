93a51ab48bfe120ba949a14d461b644694f588f1
valerio-figueira <j.valerio.figueira@gmail.com>
2022-08-18 10:42:20 -0300

    updating code

--- script/script.js
+++ script/script.js
@@ -37,6 +37,18 @@ function plusSlides(n) {
 showSlides(slideIndex += n);
 }
 
+let dot = document.querySelectorAll(".dot");
+dot[0].addEventListener("click", () => currentSlide(1));
+dot[1].addEventListener("click", () => currentSlide(2));
+dot[2].addEventListener("click", () => currentSlide(3));
+
+let prev = document.querySelector(".slideshow-container .prev");
+let next = document.querySelector(".slideshow-container .next");
+prev.addEventListener("click", () => plusSlides(-1));
+next.addEventListener("click", () => plusSlides(1));
+
+
+
 // Thumbnail image controls
 function currentSlide(n) {
 showSlides(slideIndex = n);
@@ -56,7 +68,7 @@ for (i = 0; i < dots.length; i++) {
 }
 slides[slideIndex-1].style.display = "block";
 dots[slideIndex-1].className += " active";
-} 
+}
 
 
 
