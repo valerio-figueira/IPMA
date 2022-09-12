f71dd8e81bc67ee599541d111071359f1a298992
valerio-figueira <j.valerio.figueira@gmail.com>
2022-09-12 13:18:53 -0300

    new doctors inserted

--- script/guiamedico/Cardiologia.js
+++ script/guiamedico/Cardiologia.js
@@ -184,6 +184,496 @@ export default class Cardiologia{
                 bairro: "Osvaldo Rezende",
                 telefone: "34 3236-6600"
             },
+            {
+                tipo: "Médico",
+                cod: "41706",
+                nome: "Antonio Donizette de Sena Pereira",
+                especialidade: "Cardiologia",
+                localidade: "Uberlandia - MG",
+                endereço: "R. Rafael Marino Neto, Nº 600",
+                bairro: "Jardim Karaiba",
+                telefone: "34 3256-5903"
+            },
+            {
+                tipo: "Médico",
+                cod: "26089",
+                nome: "Adriana de Oliveira Lima",
+                especialidade: "Cardiologia",
+                localidade: "Uberlandia - MG",
+                endereço: "R. Maques Povoa, Nº 88",
+                bairro: "Osvaldo Rezende",
+                telefone: "34 3236-6600 / 34 3214-0603"
+            },
+            {
+                tipo: "Médico",
+                cod: "45215",
+                nome: "Alfredo Jose Borges da Cunha",
+                especialidade: "Cardiologia",
+                localidade: "Uberlandia - MG",
+                endereço: "Av. Getulio Vargas, Nº 194",
+                bairro: "Martins",
+                telefone: "34 3255-5001"
+            },
+            {
+                tipo: "Médico",
+                cod: "56910",
+                nome: "Ana Beatriz Ferreira Jorge Carrijo",
+                especialidade: "Cardiologia",
+                localidade: "Uberlandia - MG",
+                endereço: "Av. Vasconcelos Costa, Nº 962",
+                bairro: "Martins",
+                telefone: "34 3239-0342 / 34 3211-7175"
+            },
+            {
+                tipo: "Médico",
+                cod: "353235",
+                nome: "Andre Luis Cautella Navarro",
+                especialidade: "Cardiologia",
+                localidade: "Uberlandia - MG",
+                endereço: "R. Rafael Marino Neto, Nº 600",
+                bairro: "Jardim Karaiba",
+                telefone: "34 3255-5003 / 34 3255-5001"
+            },
+            {
+                tipo: "Médico",
+                cod: "353235",
+                nome: "Andre Luis Cautella Navarro",
+                especialidade: "Cardiologia",
+                localidade: "Uberlandia - MG",
+                endereço: "Av. Getulio Vargas, Nº 194",
+                bairro: "Martins",
+                telefone: "34 3255-5001 / 34 3255-5003"
+            },
+            {
+                tipo: "Médico",
+                cod: "25670",
+                nome: "Andrea Gabaldo de Oliveira Grama Menezes",
+                especialidade: "Cardiologia",
+                localidade: "Uberlandia - MG",
+                endereço: "R. Santa Helena Nº 609",
+                bairro: "Tabajaras",
+                telefone: "34 3230-5555"
+            },
+            {
+                tipo: "Médico",
+                cod: "56515",
+                nome: "Antonio Daniel Araujo Duarte",
+                especialidade: "Cardiologia",
+                localidade: "Uberlandia - MG",
+                endereço: "R. Getulio Vargas Nº 841",
+                bairro: "Tabajaras",
+                telefone: "34 3015-3991 / 34 99873-3737"
+            },
+            {
+                tipo: "Médico",
+                cod: "12440",
+                nome: "Asilton Alves Pereira",
+                especialidade: "Cardiologia",
+                localidade: "Uberlandia - MG",
+                endereço: "Av. Joao Naves de Avila, Nº 644",
+                bairro: "Centro",
+                telefone: "34 3234-8740 / 34 3234-8767"
+            },
+            {
+                tipo: "Médico",
+                cod: "11667",
+                nome: "Bauer Dias da Silva",
+                especialidade: "Cardiologia",
+                localidade: "Uberlandia - MG",
+                endereço: "R. Tenente Virmondes, Nº 115",
+                bairro: "Centro",
+                telefone: "34 3210-8474 / 34 3253-0140"
+            },
+            {
+                tipo: "Médico",
+                cod: "19377",
+                nome: "Bruno Abelardo Debs",
+                especialidade: "Cardiologia",
+                localidade: "Uberlandia - MG",
+                endereço: "Av. Getúlio Vargas, Nº 250",
+                bairro: "Martins",
+                telefone: "34 3235-3500 / 34 3235-3840"
+            },
+            {
+                tipo: "Médico",
+                cod: "4384",
+                nome: "Castinaldo Brasil dos Santos",
+                especialidade: "Cardiologia",
+                localidade: "Uberlandia - MG",
+                endereço: "Av. Vasconcelos Costa, Nº 962",
+                bairro: "Martins",
+                telefone: "34 3239-0233 / 34 3239-0197"
+            },
+            {
+                tipo: "Médico",
+                cod: "34331",
+                nome: "Cesar Augusto Saldanha Rosa",
+                especialidade: "Cardiologia",
+                localidade: "Uberlandia - MG",
+                endereço: "Av. Getulio Vargas, Nº 194",
+                bairro: "Martins",
+                telefone: "34 3255-5001 / 34 3255-5003"
+            },
+            {
+                tipo: "Médico",
+                cod: "16836",
+                nome: "Claudio Ceccon",
+                especialidade: "Cardiologia",
+                localidade: "Uberlandia - MG",
+                endereço: "Av. Joao Pinheiro, Nº 289",
+                bairro: "Centro",
+                telefone: "34 3236-4520 / 34 3239-6045"
+            },
+            {
+                tipo: "Médico",
+                cod: "29012",
+                nome: "Cleber Augusto de Oliveira e Souza",
+                especialidade: "Cardiologia",
+                localidade: "Uberlandia - MG",
+                endereço: "R. Santa Helena, Nº 609",
+                bairro: "Tabajaras",
+                telefone: "34 3230-5555"
+            },
+            {
+                tipo: "Médico",
+                cod: "61493",
+                nome: "Conrado Lelis Ceccon",
+                especialidade: "Cardiologia",
+                localidade: "Uberlandia - MG",
+                endereço: "Av. Cipriano Del Favero, Nº 32",
+                bairro: "Centro",
+                telefone: "34 3235-6110 / 34 99898-6110"
+            },
+            {
+                tipo: "Médico",
+                cod: "22633",
+                nome: "Cornelio Lemos Debs",
+                especialidade: "Cardiologia",
+                localidade: "Uberlandia - MG",
+                endereço: "Av. Getulio Vargas, Nº 909",
+                bairro: "Centro",
+                telefone: "34 3228-9000"
+            },
+            {
+                tipo: "Médico",
+                cod: "26216",
+                nome: "Eder Naves Duarte",
+                especialidade: "Cardiologia",
+                localidade: "Uberlandia - MG",
+                endereço: "Av. Raulino Cotta Pacheco, Nº 590",
+                bairro: "Osvaldo Rezende",
+                telefone: "34 3226-6600"
+            },
+            {
+                tipo: "Médico",
+                cod: "32155",
+                nome: "Eduardo Ferreira Jorge",
+                especialidade: "Cardiologia",
+                localidade: "Uberlandia - MG",
+                endereço: "Av. Vasconcelos Costa, Nº 962",
+                bairro: "Osvaldo Rezende",
+                telefone: "34 3277-9877 / 34 99677-8330"
+            },
+            {
+                tipo: "Médico",
+                cod: "19953",
+                nome: "Emerson Clayton Borges",
+                especialidade: "Cardiologia",
+                localidade: "Uberlandia - MG",
+                endereço: "R. Rafael Marino Neto, Nº 600",
+                bairro: "Jardim Karaiba",
+                telefone: "34 3256-5900"
+            },
+            {
+                tipo: "Médico",
+                cod: "46103",
+                nome: "Felipe Augusto de Oliveira Souza",
+                especialidade: "Cardiologia",
+                localidade: "Uberlandia - MG",
+                endereço: "Av. Rondon Pacheco, Nº 2250",
+                bairro: "Saraiva",
+                telefone: "34 3234-0092"
+            },
+            {
+                tipo: "Médico",
+                cod: "38697",
+                nome: "Fernando Abrao Garcia",
+                especialidade: "Cardiologia",
+                localidade: "Uberlandia - MG",
+                endereço: "Av. Getulio Vargas, Nº 1666",
+                bairro: "Martins",
+                telefone: "34 3236-5766"
+            },
+            {
+                tipo: "Médico",
+                cod: "38697",
+                nome: "Fernando Abrao Garcia",
+                especialidade: "Cardiologia",
+                localidade: "Uberlandia - MG",
+                endereço: "R. Araxa Nº 114",
+                bairro: "Osvaldo Rezende",
+                telefone: "34 3305-6189 / 34 99173-0568"
+            },
+            {
+                tipo: "Médico",
+                cod: "31249",
+                nome: "Jaide Murilo Ferreira da Silva",
+                especialidade: "Cardiologia",
+                localidade: "Uberlandia - MG",
+                endereço: "Av. Getulio Vargas, Nº 1835",
+                bairro: "Centro",
+                telefone: "34 3229-4141"
+            },
+            {
+                tipo: "Médico",
+                cod: "42026",
+                nome: "Joao Lucas Oconnell",
+                especialidade: "Cardiologia",
+                localidade: "Uberlandia - MG",
+                endereço: "Av. Francisco Ribeiro, Nº 1140",
+                bairro: "Santa Monica",
+                telefone: "34 3215-0415 / 34 99678-9213"
+            },
+            {
+                tipo: "Médico",
+                cod: "32090",
+                nome: "Jose Rafael Vieira Junior",
+                especialidade: "Cardiologia",
+                localidade: "Uberlandia - MG",
+                endereço: "R. Santa Helena, Nº 576",
+                bairro: "Centro",
+                telefone: "34 3228-2033 / 34 3228-2040"
+            },
+            {
+                tipo: "Médico",
+                cod: "40971",
+                nome: "Luciano Martins da Silva",
+                especialidade: "Cardiologia",
+                localidade: "Uberlandia - MG",
+                endereço: "R. Santa Helena, Nº 609",
+                bairro: "Tabajaras",
+                telefone: "34 3230-5555"
+            },
+            {
+                tipo: "Médico",
+                cod: "40057",
+                nome: "Marcelo Carrijo Franco",
+                especialidade: "Cardiologia",
+                localidade: "Uberlandia - MG",
+                endereço: "Av. Cipriano Del Favero, Nº 32",
+                bairro: "Centro",
+                telefone: "34 3236-3632 / 34 99660-3632"
+            },
+            {
+                tipo: "Médico",
+                cod: "43691",
+                nome: "Marcelo Kleiman Arantes",
+                especialidade: "Cardiologia",
+                localidade: "Uberlandia - MG",
+                endereço: "R. Marques Povoa, Nº 88",
+                bairro: "Osvaldo Rezende",
+                telefone: " 34 3236-6600"
+            },
+            {
+                tipo: "Médico",
+                cod: "29702",
+                nome: "Marco Tulio Oliveira Rezende",
+                especialidade: "Cardiologia",
+                localidade: "Uberlandia - MG",
+                endereço: "R. Rafael Marino Neto, Nº 600",
+                bairro: "Jardim Karaiba",
+                telefone: "34 3255-0001 / 34 3255-5003"
+            },
+            {
+                tipo: "Médico",
+                cod: "29702",
+                nome: "Marco Tulio Oliveira Rezende",
+                especialidade: "Cardiologia",
+                localidade: "Uberlandia - MG",
+                endereço: "Av. Getulio Vargas, Nº 194",
+                bairro: "Centro",
+                telefone: "34 3255-5001 / 34 3255-5003"
+            },
+            {
+                tipo: "Médico",
+                cod: "49462",
+                nome: "Marcos Antonio Sahium Junior",
+                especialidade: "Cardiologia",
+                localidade: "Uberlandia - MG",
+                endereço: "R. Cipriano Del Favero, Nº 32",
+                bairro: "Centro",
+                telefone: "34 3235-6110"
+            },
+            {
+                tipo: "Médico",
+                cod: "49462",
+                nome: "Marcos Antonio Sahium Junior",
+                especialidade: "Cardiologia",
+                localidade: "Uberlandia - MG",
+                endereço: "R. Santa Helena, Nº 609",
+                bairro: "Tabajaras",
+                telefone: "34 3230-5555"
+            },
+            {
+                tipo: "Médico",
+                cod: "56185",
+                nome: "Marcos Goncalves Ferreira da Silva",
+                especialidade: "Cardiologia",
+                localidade: "Uberlandia - MG",
+                endereço: "R. Marques Povoa, Nº 88",
+                bairro: "Osvaldo Rezende",
+                telefone: "34 3236-6600"
+            },
+            {
+                tipo: "Médico",
+                cod: "22099",
+                nome: "Maria Auxiliadora Vedovato de Oliveira",
+                especialidade: "Cardiologia",
+                localidade: "Uberlandia - MG",
+                endereço: "Av. Getulio Vargas, Nº 194",
+                bairro: "Martins",
+                telefone: "34 3255-5001"
+            },
+            {
+                tipo: "Médico",
+                cod: "47851",
+                nome: "Mariana Ribeiro Severino Faria Barcelos",
+                especialidade: "Cardiologia",
+                localidade: "Uberlandia - MG",
+                endereço: "R. Santa Helena, Nº 609",
+                bairro: "Tabajaras",
+                telefone: "34 3230-5555"
+            },
+            {
+                tipo: "Médico",
+                cod: "28533",
+                nome: "Miriam Damiao Gomes Seabra",
+                especialidade: "Cardiologia",
+                localidade: "Uberlandia - MG",
+                endereço: "Av. Getulio Vargas, Nº 194",
+                bairro: "Martins",
+                telefone: "34 3255-5001 / 34 3255-5003"
+            },
+            {
+                tipo: "Médico",
+                cod: "30191",
+                nome: "Petronio Rangel Salvador Junior",
+                especialidade: "Cardiologia",
+                localidade: "Uberlandia - MG",
+                endereço: "R. Goias, Nº 589",
+                bairro: "Centro",
+                telefone: "34 3216-8611"
+            },
+            {
+                tipo: "Médico",
+                cod: "54055",
+                nome: "Rafael Macedo Mustafe",
+                especialidade: "Cardiologia",
+                localidade: "Uberlandia - MG",
+                endereço: "R. Cipriano Del Favero, Nº 32",
+                bairro: "Centro",
+                telefone: "34 3235-6110 / 34 99898-6110"
+            },
+            {
+                tipo: "Médico",
+                cod: "42694",
+                nome: "Rodrigo Penha de Almeida",
+                especialidade: "Cardiologia",
+                localidade: "Uberlandia - MG",
+                endereço: "R. Francisco Ribeiro, Nº 1111",
+                bairro: "Santa Monica",
+                telefone: "34 3230-6000 / 3230-6066"
+            },
+            {
+                tipo: "Médico",
+                cod: "42694",
+                nome: "Rodrigo Penha de Almeida",
+                especialidade: "Cardiologia",
+                localidade: "Uberlandia - MG",
+                endereço: "Av. Francisco Ribeiro, Nº 1140",
+                bairro: "Santa Monica",
+                telefone: "34 3239-2200"
+            },
+            {
+                tipo: "Médico",
+                cod: "19770",
+                nome: "Rogerio Ferreira Abdulmassih",
+                especialidade: "Cardiologia",
+                localidade: "Uberlandia - MG",
+                endereço: "Av. Vasconcelos Costa, Nº 962",
+                bairro: "Presidente Roosevelt",
+                telefone: "34 3239-0198"
+            },
+            {
+                tipo: "Médico",
+                cod: "35301",
+                nome: "Roseli Maria de Oliveira Carvalho",
+                especialidade: "Cardiologia",
+                localidade: "Uberlandia - MG",
+                endereço: "Av. Getulio Vargas, Nº 194",
+                bairro: "Martins",
+                telefone: "34 3255-5001 / 34 3255-5003"
+            },
+            {
+                tipo: "Médico",
+                cod: "18336",
+                nome: "Ruberval Rodrigues Santos",
+                especialidade: "Cardiologia",
+                localidade: "Uberlandia - MG",
+                endereço: "Av. Vasconcelos Costa, Nº 967",
+                bairro: "Osvaldo Rezende",
+                telefone: "34 3236-0022"
+            },
+            {
+                tipo: "Médico",
+                cod: "19810",
+                nome: "Samir Seme Arab Reis",
+                especialidade: "Cardiologia",
+                localidade: "Uberlandia - MG",
+                endereço: "R. Rafael Marino Neto, Nº 600",
+                bairro: "Jardim Karaiba",
+                telefone: "34 3256-5900 / 34 9692-7694"
+            },
+            {
+                tipo: "Médico",
+                cod: "30039",
+                nome: "Telemaco Luiz da Silva Junior",
+                especialidade: "Cardiologia",
+                localidade: "Uberlandia - MG",
+                endereço: "Av. Getulio Vargas, Nº 1835",
+                bairro: "Centro",
+                telefone: "34 3229-4141"
+            },
+            {
+                tipo: "Médico",
+                cod: "51962",
+                nome: "Thiago Machado de Lima Mota",
+                especialidade: "Cardiologia",
+                localidade: "Uberlandia - MG",
+                endereço: "R. Bernardo Cupertino, Nº 365",
+                bairro: "Martins",
+                telefone: "34 4101-0506"
+            },
+            {
+                tipo: "Médico",
+                cod: "20074",
+                nome: "Vilmar Jose Pereira",
+                especialidade: "Cardiologia",
+                localidade: "Uberlandia - MG",
+                endereço: "R. Rafael Marino Neto, Nº 600",
+                bairro: "Jardim Karaiba",
+                telefone: "34 3257-1400"
+            },
+            {
+                tipo: "Médico",
+                cod: "40051",
+                nome: "Wesley Rodrigo de Oliveira",
+                especialidade: "Cardiologia",
+                localidade: "Uberlandia - MG",
+                endereço: "Av. Getulio Vargas, Nº 194",
+                bairro: "Martins",
+                telefone: "34 3255-5003 / 34 3255-5001"
+            },
         ];
     };
 };
